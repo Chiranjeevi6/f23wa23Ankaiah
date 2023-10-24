69ed0a090cd9fb5485301c4fa20ebdd1cb17a1e8
Chiranjeevi6 <chiranjeeviankaiah6@gmail.com>
2023-10-24 15:39:39 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chiranjeevi Ankaiah' });
+  res.render('mydata', { title: 'Chiranjeevi Ankaiah' });
 });
 
 module.exports = router;
